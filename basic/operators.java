153286f786c155b95962ade3ca814b72068e02ec
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-01-25 19:25:58 +0530

    Create operators.java

---
+++ basic/operators.java
@@ -0,0 +1,8 @@
+public class operators {
+    public static void main(String[] args) {
+        int a=2;
+        int b=4;
+        int c=a+b;
+        System.out.println("summation of a and b is "+ c);
+    }
+}
