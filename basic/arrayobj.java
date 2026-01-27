0546d18afe537265c8f3f7b252fc6d1647e92419
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-01-27 20:36:16 +0530

    Update arrayobj.java

--- basic/arrayobj.java
+++ basic/arrayobj.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class arrayobj {
+public class array {
     public static void main(String[] args) {
        Scanner sc = new Scanner(System.in);
        System.out.println("enter the length oof an array ");
@@ -16,6 +16,12 @@ public class arrayobj {
 
         double average=getaverage(userarray);
         System.out.println("average of array is "+ average);
+
+        int[] sortedarray= getsortedarray(userarray);
+        System.out.print("sorted  arra is");
+        for(int i=0;i<sortedarray.length;i++){
+            System.out.print(sortedarray[i]);
+        }
 
     }
     //method to get user values 
@@ -41,5 +47,19 @@ public class arrayobj {
     public static double getaverage(int[] array){
         double avg=(double)getsumelements(array)/array.length;
         return avg;
+    }
+    public static int[] getsortedarray(int[] orgarray){
+        int[] array = orgarray.clone();
+        for(int i=0;i<array.length;i++){
+            for(int j=i+1;j<array.length;j++){
+                if(array[i]>array[j]){
+                    int temp=array[j];
+                    array[j]=array[i];
+                    array[i]=temp;
+                }
+
+            }
+        }
+        return array;
     }
 }
