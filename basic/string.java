cc570696763a3b827f3d6d81655b74c39652f56f
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-01-28 19:49:06 +0530

    Update string.java

--- basic/string.java
+++ basic/string.java
@@ -9,6 +9,15 @@ public class string{
         int n1=45;
         System.out.println(s1+" "+s2+" "+n1);
         //when an int is added with string it treats int as a string and it just concatinates int to a string
+        String s1 = "Java";               // In String Constant Pool
+        String s2 = new String("Java");   // In Heap Memory
+
+        // Using == (Address Comparison)
+        System.out.println(s1 == s2);      // Output: false 
+        // (Because one is in the Pool and the other is in the Heap)
+        // Using .equals() (Content Comparison)
+        System.out.println(s1.equals(s2)); // Output: true 
+        // (Because both contain the characters 'J', 'a', 'v', 'a')
     }
 
     
