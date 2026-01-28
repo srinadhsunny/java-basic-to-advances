3ca94a000a506452e3cbeb0364bda6a00a8235a6
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-01-28 19:42:18 +0530

    Create string.java

---
+++ basic/string.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class string{
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        String s1="its my fyst string";
+        String s2="its my secound string"; 
+        //adding two strings result in a concatinated string 
+        System.out.println(s1+ " "+s2);
+        int n1=45;
+        System.out.println(s1+" "+s2+" "+n1);
+        //when an int is added with string it treats int as a string and it just concatinates int to a string
+    }
+
+    
+}
