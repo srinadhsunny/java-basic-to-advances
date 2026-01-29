f5b5b068936e08e2953b76552a0c516325789dfe
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-01-29 20:44:40 +0530

    Create main.java

---
+++ basic/grocerymart/main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        grocerymart cust1 = new grocerymart();
+        cust1.setname("sunny");
+        cust1.setadres("hyd");
+        cust1.setphnno(987654321);
+        cust1.setacountbal(500);
+        System.out.println("enter the amount");
+        int amt=sc.nextInt();
+
+        cust1.shop(amt);
+    }
+}
