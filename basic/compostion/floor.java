378570f475e6749ce52f4b8200bf8457a225bcc4
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-02-01 21:38:17 +0530

    Create floor.java

---
+++ basic/compostion/floor.java
@@ -0,0 +1,16 @@
+public class floor {
+    private String floorno;
+
+    public floor(String floorno) {
+        this.floorno = floorno;
+    }
+
+    public String getFloorno() {
+        return floorno;
+    }
+
+    public void setFloorno(String floorno) {
+        this.floorno = floorno;
+    }
+    
+}
