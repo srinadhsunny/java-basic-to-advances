924ca462043e676769fb92fcdc96b7c8820127a9
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-02-01 21:39:05 +0530

    Create Section.java

---
+++ basic/compostion/Section.java
@@ -0,0 +1,18 @@
+public class Section {
+    private Student Student;
+
+    public Section(Student student) {
+        Student = student;
+    }
+
+    public Student getStudent() {
+        return Student;
+    }
+
+    
+    public void setStudent(Student student) {
+        Student = student;
+    }
+    
+    
+}
