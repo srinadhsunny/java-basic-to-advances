443303bad70bb44a6d91a8aebf0da97d5b7cc9d4
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-02-01 14:12:02 +0530

    Create Building.java

---
+++ basic/refernces_vs_object_vs_instamces_class/Building.java
@@ -0,0 +1,12 @@
+public class Building{
+    private String color;
+    public  Building(String color){
+        this.color=color;
+    }
+    public String getcolor(){
+        return this.color;
+    }
+    public void setcolor(String color){
+        this.color=color;
+    }
+}
