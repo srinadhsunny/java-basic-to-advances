f9bbba38cf1ef79123d979928fcf28fd68e8de8e
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-02-02 21:54:48 +0530

    Create main.java

---
+++ basic/inheritances/main.java
@@ -0,0 +1,11 @@
+public class main{
+    
+    public static void main(String[] args){
+        auto cs = new auto("red","12hp",45,2026);
+        car c1 = new car("red","12h",45,2026,"yes",7);
+        c1.carmovement();
+        c1.movement();
+        cs.movement();
+    }
+    
+}
