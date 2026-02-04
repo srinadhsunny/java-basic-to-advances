e5cac6acfbad4a670bd634e8309911aa7f56f5fd
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-02-04 21:18:36 +0530

    Create Account.java

---
+++ basic/revision/Bank/Account.java
@@ -0,0 +1,9 @@
+public class main {
+
+    public static void main(String[] args) {
+        Account a1 = new Account();
+        a1.deposit(5000);
+        a1.withdraw(3000);
+        a1.withdraw(10000);
+    }
+}
