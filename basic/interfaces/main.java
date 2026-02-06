adde9bbdf11c7845691e513b48602e7dce184e0f
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-02-06 18:52:47 +0530

    Update main.java

--- basic/interfaces/main.java
+++ basic/interfaces/main.java
@@ -1,7 +1,7 @@
 public class main {
     public static void main(String[] args) {
-        Icar myCar;//interface instances is created 
-        myCar  = new Audi(true, "petrol");//myCar instances is used to assign the differnet cars(classes) to it 
+        Icar myCar;//interface refernces is created 
+        myCar  = new Audi(true, "petrol");//myCar refernces is used to assign the differnet cars(classes) to it 
         myCar.Engine();
         myCar.Fuel();
         myCar= new Benz(false, "Desil");//class(car) is changed to Benz
