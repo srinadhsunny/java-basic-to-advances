4217607b12c61051c61539814bf5a084253b0c9c
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-02-07 20:36:15 +0530

    Create Main.java

---
+++ basic/abstract_class/prepchallenge/Main.java
@@ -0,0 +1,8 @@
+public class Main{
+    public static void main(String[] args){
+        A a1 = new A(75, 75, 75);
+        B b1 = new B(60, 60, 75, 75);
+        a1.getPercentage();
+        b1.getPercentage();
+    }
+}
