6e3eb212817ef476b0947a0de69aab07fa51da98
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-02-09 21:27:54 +0530

    Create main.java

---
+++ JAVA8 features/fyst./main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        calc c =(a,b)->{
+            return a+b;
+        };
+    System.out.println(c.add(10, 20));
+    System.out.println(c.add(32,34));
+    }
+}
