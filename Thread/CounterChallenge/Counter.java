85572e7f55574bc78b93bbf6f649a95c567f1fa4
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-02-09 12:14:28 +0530

    Update Counter.java

--- Thread/CounterChallenge/Counter.java
+++ Thread/CounterChallenge/Counter.java
@@ -11,3 +11,7 @@ public class Counter  extends Thread{
 
     
 }
+/*"I've worked with shared resources where multiple threads incremented a common variable.
+I observed a Race Condition where the final count was lower than expected because of Lost Updates.
+ I resolved this by using the synchronized keyword to ensure atomicity, and used join() to ensure 
+      the main thread waited for the completion of worker threads."*/
