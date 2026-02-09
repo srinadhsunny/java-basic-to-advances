afd1a0842c5d91a9f010b7a08b0fe9c63962795e
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-02-09 22:03:49 +0530

    Create LambdaLeap.java

---
+++ JAVA8 features/LambdaLeap.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;;
+interface testLeap {
+    public boolean test(int n);
+}
+public class LambdaLeapp {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the year ");
+        int n = sc.nextInt();
+        testLeap t = (year)->{
+            if((year%400==0)||(year%4==0)&&(year%100!=0)){
+                return true;
+            }else{
+                return false;
+            }
+        };
+        if (t.test(n)) {
+            System.out.println("its a leapyear");
+        } else {
+            System.out.println("its not a leap year");
+        }
+        
+    }
+}
