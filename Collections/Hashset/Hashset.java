43850400420aedf6d9de7c2edf0f19d80d5f0544
srinadhsunny <164844128+srinadhsunny@users.noreply.github.com>
2026-02-14 12:36:30 +0530

    Update Hashset.java

--- Collections/Hashset/Hashset.java
+++ Collections/Hashset/Hashset.java
@@ -12,16 +12,20 @@ public class Hashset{
         //intersection of value 1 and value 2
         Set<Integer> intersection = new HashSet<>(value1);
         System.out.println("intersection");
+        intersection.retainAll(value2);
         System.out.println(intersection);
         //union set
         Set<Integer> union = new HashSet<>(value1);
         System.out.println("union");
+        union.addAll(value2);
         System.out.println(union);
         //differnces 1 value 1- value 2
         Set<Integer> diff1 = new HashSet<>(value1);
+        diff1.removeAll(value2);
         System.out.println("differneces 1"+ diff1);
         //differences  2
         Set<Integer> diff2 = new HashSet<>(value1);
+        diff2.removeAll(value2);
         System.out.println("diff2 "+ diff2);
 
 
